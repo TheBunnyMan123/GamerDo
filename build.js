f8465fd16ce1bae81ffb462e477ee2a0af41df76
TheBunnyMan123 <bunny@tkbunny.net>
2025-04-27 15:22:53 -0500

    Enhance build script

--- build.js
+++ build.js
@@ -6,17 +6,9 @@ import * as sass from "sass";
 import tsify from "tsify";
 const scriptRegex = /<script\s+?src="?bundle\.js"?>\s*?<\/script>/g
 const cssRegex = /<link rel="stylesheet" href="style\.css" ?\/?>/g
+const whitespaceRegex = /\s+/g;
 
 try{fs.mkdirSync("./out", true);}catch(e){console.log(e);}
-
-function inline(inFile, out) {
-   console.log(fs.readFileSync("./tmp/bundle.js").available);
-   try{fs.unlinkSync(out);}catch(e){console.log(e);}
-   fs.writeFileSync(
-      out,
-      fs.readFileSync(inFile).toString().replace(scriptRegex, `<script type="text/javascript">${fs.readFileSync("./tmp/bundle.js").toString()}</script>`)
-   );
-}
 
 (async function() {
    await browserify("./src/js/main.ts")
@@ -26,7 +18,7 @@ function inline(inFile, out) {
       if (err) {
          console.error(err);
       } else {
-         fs.writeFileSync("./out/bundle.html", fs.readFileSync("./src/index.html").toString().replace(cssRegex, `<style>${
+         fs.writeFileSync("./out/bundle.html", fs.readFileSync("./src/index.html").toString().replace(whitespaceRegex, " ").replace(cssRegex, `<style>${
             sass.compile("./src/css/style.scss", {style: "compressed"}).css
          }</style>`).replace(scriptRegex, `<script>${
             UglifyJS.minify(buff.toString()).code
